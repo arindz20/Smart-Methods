1ace8a67fdf31be397f05e471235ec904f5253b2
Ahmed <66774628+arindz20@users.noreply.github.com>
2020-06-22 09:56:41 +0300

    Add files via upload

---
+++ stepper_motor1.ino
@@ -0,0 +1,14 @@
+//#include <Stepper.h>
+//int spr = 1;
+//Stepper stepp(spr, 2,5,3,4);
+
+void setup() {
+//stepp.setSpeed(10);
+}
+
+void loop() {
+/*stepp.step(spr);
+delay(3000);
+stepp.step(-1);
+delay(1000); */
+}
